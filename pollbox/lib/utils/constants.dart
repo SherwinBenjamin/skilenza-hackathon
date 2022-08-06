15f087bbfebbf720c43975096a212dd9e7b8ee5e
sb3171 <72021968+sb3171@users.noreply.github.com>
2022-08-06 17:31:10 +0530

    polling and survey

---
+++ pollbox/lib/utils/constants.dart
@@ -0,0 +1,10 @@
+String infura_url =
+    "https://kovan.infura.io/v3/9ea83adfc55b4869a2fbb2f9cf9b383d";
+
+String owner_private_key =
+    "f374c874241d75ec73b4482f1dc34f042d8f90752cbeb8f4a8325a984bb3bdfd";
+
+String voter_private_key =
+    "a53babc7356ac3329795c3ae9f0e531c4bc8e67a0f778ecce90eb175a04f1bb1";
+
+String contractAddress1 = "0x4be3E2B39120386e499D03A9fDe069F11D6566e2";
